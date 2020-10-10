1fb6a43e19b5d891e3d60932621358ce9d8bc660
Ahmed Moeen Serdah <35952861+aserdah@users.noreply.github.com>
2020-10-10 22:36:35 +0300

    Initial Commit

---
+++ Onboarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onboarding
+//
+//  Created by Ahmed Serdah on 10/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Ahmed Serdah on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
